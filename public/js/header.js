7aad779cc55ea1ea5dc44d5be82b58612357fdd7
Akash <akashbojja3@gmail.com>
2025-11-30 11:16:19 +0530

    Added Images and Working post feed

--- public/js/header.js
+++ public/js/header.js
@@ -1,3 +1,7 @@
 document.querySelector('.loginbtn').addEventListener('click', () =>{
     window.location.href = '/login';
 });
+
+document.querySelector('.signupbtn').addEventListener('click', () =>{
+    window.location.href = '/signup';
+});
